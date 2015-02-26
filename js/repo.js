1714733c57a5d99aa6d33a504cad5874e9507ab3
Earl Escueta <earl909@yahoo.com>
2015-02-26 15:42:29 -0800

    Added descriptions/links, google form embed

--- js/repo.js
+++ js/repo.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
 	$("<h1>BD2K Center of Excellence @ UCLA</h1><h2>Software Repositories</h2>").hide().appendTo("#title").fadeIn(1000);
+
+	// Initialize the plugin
+      $('#popup_form').popup();
 });
